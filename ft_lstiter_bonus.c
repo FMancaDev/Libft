5d39ac8e93161da9bf60fa4317f9f622740fb28a
Fode Manca <fomanca@c2r3s9.42porto.com>
2025-10-20 13:31:37 +0100

    iterate over list applying a function

---
+++ ft_lstiter_bonus.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstiter_bonus.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fomanca <fomanca@student.42porto.com>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/15 17:40:24 by fomanca           #+#    #+#             */
+/*   Updated: 2025/10/16 13:01:18 by fomanca          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+void	ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	t_list	*current;
+
+	if (!lst || !f)
+		return ;
+	current = lst;
+	while (current != NULL)
+	{
+		f(current->content);
+		current = current->next;
+	}
+}
