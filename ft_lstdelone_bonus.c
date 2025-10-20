8682964b76664c94e4ef744fd931eb1b579ee72a
Fode Manca <fomanca@c2r3s9.42porto.com>
2025-10-20 13:31:15 +0100

    delete a single node from the list

---
+++ ft_lstdelone_bonus.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdelone_bonus.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fomanca <fomanca@student.42porto.com>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/15 16:03:37 by fomanca           #+#    #+#             */
+/*   Updated: 2025/10/16 12:47:46 by fomanca          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
+{
+	if (!lst || !del)
+		return ;
+	del(lst->content);
+	free(lst);
+}
