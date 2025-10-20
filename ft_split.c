49fde2e27b3df52ccaad6d1f6acef91342ab67aa
Fode Manca <fomanca@c2r3s9.42porto.com>
2025-10-20 13:38:17 +0100

    split string into array of strings

--- ft_split.c
+++ ft_split.c
@@ -3,70 +3,95 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fomanca <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: fomanca <fomanca@student.42porto.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/13 11:17:53 by fomanca           #+#    #+#             */
-/*   Updated: 2025/10/13 20:19:31 by fomanca          ###   ########.fr       */
+/*   Created: 2025/10/16 15:54:15 by fomanca           #+#    #+#             */
+/*   Updated: 2025/10/17 10:51:38 by fomanca          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static size_t	count_word(const char *s, char c)
+#include <stdlib.h>
+#include "libft.h"
+
+static size_t	count_words(const char *s, char c)
 {
 	size_t	count;
-	int		i;
 
 	count = 0;
-	i = 0;
-	while (s[i])
-	{
-		if (s[i] != c)
-		{
-			count++;
-			while (s[i] && s[i] != c)
-				s++;
-		}
-		else
-			s++;
-	}
-	return (count);
-}
-
-char	**ft_split(const char *s, char c)
-{
-	char		**words;
-	const char	*start;
-	size_t		j;
-
-	if (!s)
-		return (NULL);
-	words = malloc(sizeof(char *) * (count_word(s, c) + 1));
-	if (!words)
-		return (NULL);
-	j = 0;
 	while (*s)
 	{
 		if (*s != c)
 		{
-			start = s;
+			++count;
 			while (*s && *s != c)
-				s++;
-			words[j++] = ft_substr(start, 0, s - start);
+				++s;
 		}
 		else
-			s++;
+			++s;
 	}
-	words[j] = NULL;
-	return (words);
+	return (count);
+}
+
+static void	free_array(char **array, size_t index)
+{
+	while (index > 0)
+		free(array[--index]);
+	free(array);
+}
+
+static char	*word_separator(const char **s, char c, char **array, size_t index)
+{
+	size_t	len;
+	char	*word;
+
+	len = 0;
+	while ((*s)[len] && (*s)[len] != c)
+		++len;
+	word = ft_substr(*s, 0, len);
+	if (!word)
+	{
+		free_array(array, index);
+		return (NULL);
+	}
+	*s += len;
+	return (word);
+}
+
+char	**ft_split(const char *s, char c)
+{
+	char	**array;
+	size_t	index;
+
+	if (!s)
+		return (NULL);
+	array = malloc(sizeof(char *) * (count_words(s, c) + 1));
+	if (!array)
+		return (NULL);
+	index = 0;
+	while (*s)
+	{
+		if (*s != c)
+		{
+			array[index] = word_separator(&s, c, array, index);
+			if (!array[index])
+				return (NULL);
+			index++;
+		}
+		else
+			++s;
+	}
+	array[index] = NULL;
+	return (array);
 }
 
 /*#include <stdio.h>
 
 int main(void)
 {
-    char *str = "Hello,world,test";
-    char **result = ft_split(str, ',');
+    char *str = "Hello_world_test";
+    char **result = ft_split(str, '_');
     size_t i = 0;
 
     if (!result)
